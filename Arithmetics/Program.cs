e58e0d6634ec2e85b69703a7650d820cca9acdef
nuss <758956946@qq.com>
2018-07-04 22:43:58 +0800

    添加项目文件。

---
+++ Arithmetics/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Arithmetics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
